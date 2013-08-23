34d002e4272f6683a8b4ecbcfe24a81e93e61332
dfirst <justnew@i.ua>
2013-08-23 23:15:21 +0300

    ticket:1 static fix#2

--- testingslow/settings.py
+++ testingslow/settings.py
@@ -1,6 +1,5 @@
 # Django settings for testingslow project.
 import os
-import sys
 
 
 DEBUG = True
@@ -64,7 +63,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = sys.path[0]+'/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,6 +71,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
