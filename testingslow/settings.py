6a451b7f235ba6132976ef5d5cbea726ad2ee370
dfirst <justnew@i.ua>
2013-08-23 18:18:14 +0300

    ticket:1 static fix

--- testingslow/settings.py
+++ testingslow/settings.py
@@ -1,5 +1,8 @@
 # Django settings for testingslow project.
 import os
+import sys
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -61,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATIC_ROOT = sys.path[0]+'/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
