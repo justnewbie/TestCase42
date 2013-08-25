599ca428ea61e5d6209019bc8d13d2f3641be9ef
dfirst <justnew@i.ua>
2013-08-26 00:46:52 +0300

    ticket:8 pep8 fix

--- testapp/templatetags/admin_link.py
+++ testapp/templatetags/admin_link.py
@@ -7,8 +7,7 @@ register = template.Library()
 
 def admin_link(self):
     return urlresolvers.reverse(
-        'admin:%s_%s_change' % (self._meta.app_label, self._meta.module_name),
-                                args=(self.id,))
+        'admin:%s_%s_change' % (self._meta.app_label, self._meta.module_name), args=(self.id,))
 
 
 register.simple_tag(admin_link)
