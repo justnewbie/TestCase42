c78af72c9f74a3ab7b9bc4bd56a716c664e36c2d
dfirst <justnew@i.ua>
2013-08-26 00:24:38 +0300

    ticket:8 test fix

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from models import Person, Hook_http
+
+from models import Person, RequestLogs
 
 
 class PersonAdmin(admin.ModelAdmin):
-    fields = ('first_name', 'last_name', 'b_date', 'about', 'email', 'jabber', 'photography')
+    fields = ('first_name', 'last_name', 'b_date',
+              'about', 'email', 'jabber')
     list_display = ('first_name', 'last_name')
+
 admin.site.register(Person, PersonAdmin)
 
 
-class HookAdmin(admin.ModelAdmin):
-    fields = ('http_request',)
-    list_display = ('http_request',)
-admin.site.register(Hook_http, HookAdmin)
+class RequestLogsAdmin(admin.ModelAdmin):
+    fields = ('url', 'method', 'time_stamp')
+    list_display = ('url',)
+
+admin.site.register(RequestLogs, RequestLogsAdmin)
